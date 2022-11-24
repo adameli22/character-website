56756adc4c440e176aa642be02e46f29a6fc2c0a
adameli22 <adami19@ru.is>
2022-11-24 21:37:38 +0000

    fixing deployment issue

--- pages/portal.tsx
+++ pages/portal.tsx
@@ -1,13 +1,11 @@
 import classNames from "classnames";
 import Image from "next/image";
-import portalimg from "../public/portal.png";
+import portalimg from "../public/rickandmorty/portal.png";
 import styles from "../styles/portal.module.css";
 
 const portal = () => {
   return (
     <>
-     
-
       <div className={classNames(styles.body, styles.portal_frame)}>
         <div className={styles.portal_pulse}>
           <Image className={styles.portal_spin} src={portalimg} alt="" width="250" height="250" />
